6b0644cd914031e9c2b4acd7cc636e3e5e9285d2
frankluo123 <133156484+frankluo123@users.noreply.github.com>
2023-10-17 21:11:48 +0000

    Some initial edits

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,35 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/**
+* Sets up a route handler for POST requests
+*/ 
+router.post('/submitFoodPreferences', (req, res, next) => {
+  // Form data
+  const iceCreamFlavor = req.body.iceCreamFlavor;
+  const pizzaToppings = req.body.pizzaToppings;
+  const favoriteFruit = req.body.favoriteFruit;
+  const cuisine = req.body.cuisine;
+  const spiciness = req.body.spiciness;
+
+  // Checks if any food elements are missing
+  if (!iceCreamFlavor || !pizzaToppings || !favoriteFruit || !cuisine || !spiciness) {
+      res.status(422).send("Incomplete data!.");
+      return;
+  }
+
+
+// Debugging purposes
+console.log(req.body.iceCreamFlavor);
+console.log(req.body.pizzaToppings);
+console.log(req.body.favoriteFruit);
+console.log(req.body.cuisine);
+console.log(req.body.spiciness);
+
+// For cookie and a thank you message for the POST request
+// res.cookie('bestcookie', 'samoas', { maxAge: 900000, httpOnly: false});
+res.cookie('favoriteFruit', req.body.favoriteFruit, { maxAge: 900000, httpOnly: false });
+res.send("Thanks for sharing your food preferences! :) Your favorite fruit is: " + req.body.favoriteFruit);
+});
+
 module.exports = router;
